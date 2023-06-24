6537fc64be2d46167502cf179e955e0710965627
hrmdbipolar <hrmandr@gmail.com>
2023-06-24 18:44:09 +0300

    new changes

--- src/routes/mastering.route.js
+++ src/routes/mastering.route.js
@@ -2,7 +2,7 @@ import express from 'express';
 import * as masteringController from '../controllers/mastering.controller.js'
 const router = express.Router();
 import multer from 'multer';
-const upload = multer({ dest: 'src/uploads/' });
+const upload = multer({ dest: 'uploads/' });
 
 router.post('/masterize', upload.single('file'), masteringController.masterize);
 router.get('/getMastered', masteringController.getMasterized)
